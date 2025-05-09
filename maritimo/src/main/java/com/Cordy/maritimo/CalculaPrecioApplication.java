30e46343f471efa73ee4464aaddab2cd52864f03
Vina del Mar <vina@duoc.acad>
2025-05-09 11:57:15 -0400

    1

---
+++ maritimo/src/main/java/com/Cordy/maritimo/CalculaPrecioApplication.java
@@ -0,0 +1,13 @@
+package com.Cordy.maritimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculaPrecioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculaPrecioApplication.class, args);
+	}
+
+}
