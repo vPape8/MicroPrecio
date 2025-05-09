f27be011daa7c56a953fc7805525a63d0d538af3
Vina del Mar <vina@duoc.acad>
2025-05-09 12:21:20 -0400

    inicio creacio del modelo

---
+++ maritimo/src/main/java/com/Cordy/maritimo/Model/Barco.java
@@ -0,0 +1,14 @@
+package com.Cordy.maritimo.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "barco")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Barco {
+    
+}
